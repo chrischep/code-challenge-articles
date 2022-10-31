ce8293e552f8a830f0b0fc0b3fa28158952c7910
chrischep <christine.chepleting@student.moringaschool.com>
2022-10-31 10:04:21 +0300

    dfine methods in article.rb

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,4 +1,14 @@
 class Article
+    @@all = []
+    attr_reader :author, :magazine, :title
 
-
+    def self.all
+        @@all
+    end
+    def initialize (author, magazine, title)
+        @author = author
+        @magazine = magazine
+        @title = title
+        @@all << self
+    end
 end
