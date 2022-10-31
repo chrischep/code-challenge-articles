ecc4a218f2afd84c4f11517eeeb768f617bdbeae
chrischep <christine.chepleting@student.moringaschool.com>
2022-10-31 10:32:37 +0300

    select articles from different authors

--- app/models/author.rb
+++ app/models/author.rb
@@ -1,10 +1,16 @@
 class Author
   attr_accessor :name
-
+@@all=||
 
   def initialize(name)
     @name = name
-  
+  @@all<<self
+  end
+  def articles
+    Article.all.select{|article|article.author==self}
+  end
+  def magazines
+    article.map{|article|article.magazine}.uniq
   end
 
 
